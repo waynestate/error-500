ec7b2fbccd9a3aee0186e2f1b1961fe9ff8c5b11
Robert Vrabel <rvrabel@gmail.com>
2015-06-15 10:09:39 -0400

    initial import updating foundation classes to go off the new breakpoints from the style guide

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+
+// Load plugins
+var $ = require('gulp-load-plugins')();
+
+// Where is the app?
+var appDir = 'src/';
+
+// Where do you store your Sass files?
+var sassDir = appDir + 'scss';
+
+// Which directory should the app compile to?
+var targetAppDir = 'dist/';
+var targetResourcesDir = targetAppDir;
+
+// Which directory should Sass compile to?
+var targetCSSDir = targetResourcesDir + '';
+
+// Bower Dir
+var bowerDir = 'bower_components';
+
+// Foundation JS Dir
+var foundationJsDir = bowerDir + '/foundation/js/foundation';
+
+gulp.task('styles', function () {
+    return gulp.src(sassDir + '/500.scss')
+        .pipe($.plumber())
+        .pipe($.rubySass({
+            style: 'expanded',
+            loadPath: ['bower_components'],
+            compass: false,
+            bundleExec: true
+        }))
+        .pipe($.autoprefixer('last 5 version'))
+        .pipe($.minifyCss())
+        .pipe(gulp.dest(targetCSSDir))
+        .pipe($.size());
+});
+
+// Default task
+gulp.task('default', ['styles']);
+
+// Keep an eye on Sass, JS, for changes...
+gulp.task('watch', ['styles'], function () {
+
+    // Initialize livereload
+    $.livereload = $.livereload();
+
+    gulp.watch('500.css', function (file) {
+        $.livereload.changed(file.path);
+    });
+
+    gulp.watch(sassDir + '/**/*.scss', ['styles']);
+});
